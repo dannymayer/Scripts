b4467e52c6423fef2cd553f20c37a47955f7c467
dannymayer <dannyrmayer@gmail.com>
2020-08-24 21:07:59 -0500

    crt append_new_line and append_multiple_lines func

--- text/archive-file/archive-file.py
+++ text/archive-file/archive-file.py
@@ -1,9 +1,49 @@
 import os
 
 """
+@TODO
 1. Get Path of file to move to archive
 2. Append "#Archive" tag to copy of .md file
 3. Move appended file to /Archive Folder
 4. Delete original file
 """
 
+
+def append_new_line(file_name, text_to_append):
+    """Append given text as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+") as file_object:
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # If file is not empty then append '\n'
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        # Append text at the end of file
+        file_object.write(text_to_append)
+
+
+def append_multiple_lines(file_name, lines_to_append):
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+") as file_object:
+        appendEOL = False
+        # Move read cursor to the start of file
+        file_object.seek(0)
+        # Check if file is not empty
+        data = file_object.read(100)
+        if len(data) > 0:
+            appendEOL = True
+        # Iterate over each string in the list
+        for line in lines_to_append:
+            # If file is not empty then append '\n' before first line
+            # Always append '\n' before appending line
+            if appendEOL == True:
+                file_object.write("\n")
+            else:
+                appendEOL = True
+            # Append element at the end of file
+            file_object.write(line)
+
+
+# Append one line to a file that does not exist
+append_new_line('sample3.txt', 'This is the second line')
